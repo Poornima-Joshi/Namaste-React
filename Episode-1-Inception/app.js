7a43649c0d4001a8e1971ad54186ef596d27c889
Poornima-Joshi <poornima.joshi776@gmail.com>
2024-01-25 10:53:25 +0530

    create hello world using react

--- Episode-1-Inception/app.js
+++ Episode-1-Inception/app.js
@@ -1,3 +1,26 @@
-const heading = React.createElement("h1",{id:"heading",className:"paragraph"},"Hello world from React");
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+/**
+ * <div id="parent">
+ *    <div id="child">
+ *        <h1>This is h1 element</h1>
+ *        <h2>This is h2 element</h2>
+ *     </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [ 
+    React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading1", className: "paragraph"}, "This is h1 Element"),
+    React.createElement("h2", { id: "heading2", className: "paragraph"}, "This is h2 Element"),
+  ]),
+   React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading3", className: "paragraph"}, "This is h1 Element"),
+    React.createElement("h2", { id: "heading4", className: "paragraph"}, "This is h2 Element"),
+  ]),
+]
+);
+console.log(parent);  //this is return to object means (return the reactElement);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
