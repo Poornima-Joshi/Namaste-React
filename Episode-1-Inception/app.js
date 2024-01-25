6e91976403b84cecd7b25d50dac022fe9d6f12c0
Poornima-Joshi <poornima.joshi776@gmail.com>
2024-01-25 22:59:05 +0530

    add jsx

--- Episode-1-Inception/app.js
+++ Episode-1-Inception/app.js
@@ -8,20 +8,24 @@
  */
 import React from "react";
 import  ReactDOM  from "react-dom/client";
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [ 
-    React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "heading1", className: "paragraph"}, "This is h1 Element"),
-    React.createElement("h2", { id: "heading2", className: "paragraph"}, "This is h2 Element"),
-  ]),
-   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading3", className: "paragraph"}, "This is h1 Element"),
-    React.createElement("h2", { id: "heading4", className: "paragraph"}, "This is h2 Element"),
-  ]),
-]
-);
-console.log(parent);  //this is return to object means (return the reactElement);
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   [ 
+//     React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", { id: "heading1", className: "paragraph"}, "This is h1 Element"),
+//     React.createElement("h2", { id: "heading2", className: "paragraph"}, "This is h2 Element"),
+//   ]),
+//    React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", { id: "heading3", className: "paragraph"}, "This is h1 Element"),
+//     React.createElement("h2", { id: "heading4", className: "paragraph"}, "This is h2 Element"),
+//   ]),
+// ]
+// );                          
+
+const jsxHeading = <h1 className="head">Namaste React using JSX</h1>
+console.log(jsxHeading);
+
+//console.log(parent);  //this is return to object means (return the reactElement);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
